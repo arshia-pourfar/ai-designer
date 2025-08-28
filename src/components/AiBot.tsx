0cfd16b0a288a8b9de4e1826e130eb7430ba6493
arshiapourfar <arshiapourfar@gmail.com>
2025-08-28 23:22:45 +0330

    add requirements.txt file

--- src/components/AiBot.tsx
+++ src/components/AiBot.tsx
@@ -13,7 +13,7 @@ const AiBot: React.FC<AiBotProps> = ({ fabricCanvasRef }) => {
 
     const handleGenerate = async (prompt: string) => {
         try {
-            const response = await fetch('http://127.0.0.1:8000/generate', {
+            const response = await fetch('https://ai-designer-y9d9.onrender.com/generate', {
                 method: 'POST',
                 headers: { 'Content-Type': 'application/json' },
                 body: JSON.stringify({ prompt }),
